5e75578f7e5b30c769a99c4d939340c0219a9aaf
Christian Unger <23141@tearatai.school.nz>
2025-02-14 09:10:03 +1300

    small code  shanges

--- main/main.py
+++ main/main.py
@@ -6,12 +6,13 @@ import os.path
 class Mainloop:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1280,720))
+        self.screen = pygame.display.set_mode((1280,720), pygame.RESIZABLE)
         self.clock = pygame.time.Clock()
         icon = pygame.image.load(os.path.dirname(os.path.abspath(__file__))+'/../static/game_icon.png')
 
         pygame.display.set_icon(icon)
         self.running = True
+        self.in_menu = True
         self.main_menu()
         self.game_loop()
 
@@ -21,14 +22,19 @@ class Mainloop:
                 if event.type == pygame.QUIT:
                     self.running = False
 
-            pygame.display.flip()
+            if self.in_menu:
+                self.main_menu()
+
+            pygame.display.update()
             self.clock.tick(60)
 
     def display(self):
         pass
 
     def main_menu(self):
-        pass
+        self.menu_bg = pygame.image.load(os.path.dirname(os.path.abspath(__file__))+'/../static/crappy_bg.png')
+        self.screen.blit(self.menu_bg, (0,0))
+
 
 class Car():
     def __init__(self):

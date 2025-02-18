a8b5bcb30b5814d934eaa6aca5e19d74febae79f
Christian Unger <23141@tearatai.school.nz>
2025-02-19 12:45:06 +1300

    cleaned up code, decided on display system

--- main/main.py
+++ main/main.py
@@ -4,50 +4,61 @@ pygame.init()
 
 class Mainloop:
     def __init__(self):
-
         self.SCREEN_WIDTH = pygame.display.Info().current_w
         self.SCREEN_HEIGHT = pygame.display.Info().current_h
-
         self.clock = pygame.time.Clock()
-        icon = pygame.image.load(os.path.dirname(os.path.abspath(__file__))+'/../static/game_icon.png')
 
+        icon = pygame.image.load(os.path.dirname(os.path.abspath(__file__))+'/../static/game_icon.png')
         pygame.display.set_icon(icon)
+
         self.running = True
         self.menu_bg = pygame.image.load(os.path.dirname(os.path.abspath(__file__))+'/../static/crappy_bg.png')
         self.fullscreen = True
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        self.main_menu()
+        self.game_states = [self.main_menu]
+        self.state = self.game_states[0]
         self.game_loop()
+
+
 
     def game_loop(self):
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_F11:
-                        if self.fullscreen:
-                            self.screen = pygame.display.set_mode((self.SCREEN_WIDTH / 2, self.SCREEN_HEIGHT / 2), pygame.RESIZABLE)
-                            self.fullscreen = False
-                        else:
-                            self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-                            self.fullscreen = True
+                        self.state(3)
+
                 elif event.type == pygame.QUIT:
                     self.running = False
 
-
-                elif event.type == pygame.VIDEOEXPOSE:  
-                    self.menu_bg = pygame.transform.scale(self.menu_bg, self.screen.get_size())
-                    self.screen.blit(self.menu_bg, (0,0))
-                    pygame.display.update()
-
+                elif event.type == pygame.VIDEOEXPOSE:
+                    self.state(2)
 
             pygame.display.update()
             self.clock.tick(60)
 
-    def display(self):
-        pass
 
-    def main_menu(self):
-        self.screen.blit(self.menu_bg, (0,0))
+
+    def main_menu(self, change = 1):
+        #change, 1 = normal display, 2 = resize, 3 = fullscreen/not full
+
+        if change == 1:
+            self.screen.blit(self.menu_bg, (0,0))
+
+        if change == 2:
+            self.menu_bg = pygame.transform.scale(self.menu_bg, self.screen.get_size())
+            self.screen.blit(self.menu_bg, (0,0))
+            pygame.display.update()
+
+        if change == 3:
+            if self.fullscreen:
+                self.screen = pygame.display.set_mode((self.SCREEN_WIDTH / 2, self.SCREEN_HEIGHT / 2), pygame.RESIZABLE)
+                self.fullscreen = False
+            else:
+                self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+                self.fullscreen = True
+
+
 
 
 class Car():

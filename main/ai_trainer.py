5f958e5c7c9353bc2fef799407ae315e5208fab5
HelpMehh <christian.kiwi.unger@gmail.com>
2025-08-10 17:05:16 +1200

    started on ai

---
+++ main/ai_trainer.py
@@ -0,0 +1,27 @@
+import os
+import neat
+
+def eval_genomes():
+    def __init__():
+        pass
+
+def run(config_path):
+    config = neat.config.Config(
+        neat.DefaultGenome,
+        neat.DefaultReproduction,
+        neat.DefaultSpeciesSet,
+        neat.DefaultStagnation,
+        config_path
+    )
+
+    pop = neat.Population(config)
+    pop.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    pop.add_reporter(stats)
+    pop.run(eval_genomes, 1000)
+
+
+if __name__ is '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config-feedforward.txt')
+    run(config_path)

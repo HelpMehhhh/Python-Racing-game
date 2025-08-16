fixed rotation and got new model

main/cars.py
@@ -128,7 +128,7 @@ class Car():
     def draw(self):
         self.car = pg.image.load(os.path.dirname(os.path.abspath(__file__))+f'/../static/car_{self.color_id}.png')
         self.car = pg.transform.scale(self.car, self.game.convert_passer([2, 4], 0))
-        self.car_rect = self.car.get_rect(center=self.game.convert_passer(self.pos))
+
 
 
 
@@ -171,6 +171,7 @@ class PlayerCar(Car):
 
     def draw(self):
         super().draw()
+        self.car_rect = self.car.get_rect(center=self.game.convert_passer(self.pos))
         self.screen.blit(self.car, self.car_rect)
         pg.draw.line(self.screen, 'lime', self.game.convert_passer(self.pos), self.game.convert_passer(self.cl_points[self.target_cl_index]), 6)
         pg.draw.line(self.screen, 'red', self.game.convert_passer(self.pos), self.game.convert_passer(self.point), 6)
@@ -274,7 +275,8 @@ class AiCar(Car):
 
     def draw(self):
         super().draw()
-        self.car = pg.transform.rotate(self.car, self.rotation - self.car_angle)
+        self.car = pg.transform.rotate(self.car, float(np.degrees(self.car_angle))-float(np.degrees(self.rotation)))
+        self.car_rect = self.car.get_rect(center=self.game.convert_passer(self.pos))
         self.screen.blit(self.car, self.car_rect)
 
 